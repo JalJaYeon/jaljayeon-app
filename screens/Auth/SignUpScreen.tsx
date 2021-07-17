4bbbad26f967720ed9eb7bc1cb711e54951e0bc9
Taegeon Go <tae.gun7784@gmail.com>
2021-07-18 00:53:23 +0900

    feat(screen): add auth/signin

---
+++ screens/Auth/SignUpScreen.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
+import Header from 'components/Header';
+import Input from 'components/Input';
+import {BLACK, WHITE} from 'utils/color';
+import {fs, hp, wp} from 'utils/size';
+import {MEDIUM} from 'utils/font';
+import DropDown from 'components/Dropdown';
+import Button from 'components/Button';
+
+const SignUpScreen = () => {
+  const navigate = useNavigation();
+
+  const timeStartItems = new Array(24).fill(0).map((_, index) => ({
+    label: `${index}시`,
+    value: index,
+    key: `time${index}`,
+  }));
+
+  const minuteStartItems = new Array(60).fill(0).map((_, index) => ({
+    label: `${index}분`,
+    value: index,
+    key: `date${index}`,
+  }));
+
+  const hourItems = new Array(12).fill(0).map((_, index) => ({
+    label: `${index}시간`,
+    value: index,
+    key: `hour${index}`,
+  }));
+
+  const minuteItems = new Array(60).fill(0).map((_, index) => ({
+    label: `${index}분`,
+    value: index,
+    key: `minute${index}`,
+  }));
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+        <Header onPress={() => navigate.goBack()} title="회원가입" />
+        <Input top={hp('2.8%')} placeholder="이름" />
+        <Input top={hp('2.4%')} placeholder="아이디" />
+        <Input top={hp('2.4%')} placeholder="비밀번호" />
+        <Input top={hp('2.4%')} placeholder="몸무게 (단위: KG)" />
+        <Text style={styles.title}>평소 취침 시작 시각</Text>
+        <View style={styles.dropdownWrapper}>
+          <DropDown items={timeStartItems} />
+          <DropDown items={minuteStartItems} />
+        </View>
+        <Text style={styles.title}>평소 취침 시간</Text>
+        <View style={styles.dropdownWrapper}>
+          <DropDown items={hourItems} />
+          <DropDown items={minuteItems} />
+        </View>
+        <Button top={hp('5.1%')} label="가입" />
+      </KeyboardAwareScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: WHITE,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: fs(14),
+    color: BLACK,
+    fontFamily: MEDIUM,
+    width: wp('84.6%'),
+    marginTop: hp('5.2%'),
+    marginBottom: hp('1.9%'),
+  },
+  dropdownWrapper: {
+    flexDirection: 'row',
+    width: wp('84.6%'),
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+});
+
+export default SignUpScreen;
