7c182f7967ad2010f082e0edf99184a27169f8f9
Taegeon Go <tae.gun7784@gmail.com>
2021-07-17 19:51:05 +0900

    feat(app): add splash hide

--- App.tsx
+++ App.tsx
@@ -7,9 +7,14 @@
 
 import React from 'react';
 import {SafeAreaView, StatusBar, useColorScheme} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(() => {
+    SplashScreen.hide();
+  });
 
   return (
     <SafeAreaView>
