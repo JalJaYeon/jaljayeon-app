6facad0a6b0a81267c921939e730c90e6fb0ae73
Taegeon Go <tae.gun7784@gmail.com>
2021-07-18 02:59:17 +0900

    chore(utils): remove placeholder color

--- utils/color.ts
+++ utils/color.ts
@@ -3,5 +3,4 @@ export const BLUE = '#4170FF';
 export const BLACK = '#1D1D1D';
 export const GRAY = '#707070';
 export const LIGHTGRAY = '#DADADA';
-export const PLACEHOLDER = '#dadada';
 export const TRANSPARENT = 'transparent';
